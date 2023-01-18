da87338096860a6f6a32be3e2613dfd80bdd7afa
thallesyam <thallesyam@gmail.com>
2023-01-18 17:12:38 -0300

    feat: add extends props to img tag

--- src/components/Avatar/index.tsx
+++ src/components/Avatar/index.tsx
@@ -1,13 +1,15 @@
+import { ImgHTMLAttributes } from "react"
 import style from "./style.module.css"
 
-type IAvatar = {
+type IAvatar = ImgHTMLAttributes<HTMLImageElement> & {
   avatarUrl: string
   hasBorder?: boolean
 }
 
-export function Avatar({ avatarUrl, hasBorder = true }: IAvatar) {
+export function Avatar({ avatarUrl, hasBorder = true, ...rest }: IAvatar) {
   return (
     <img
+      {...rest}
       className={hasBorder ? style.avatarWithBorder : style.avatar}
       src={avatarUrl}
     />
